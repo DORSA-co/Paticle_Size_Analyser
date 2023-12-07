5dd9e4a32827534a0f436ffbb2ef22c2a9bcd90b
amirhossein-mlkz <amirhossein.mlkz@gmail.com>
2023-12-07 17:26:35 +0330

    csv bug fix

--- backend/Utils/dataUtils.py
+++ backend/Utils/dataUtils.py
@@ -4,5 +4,14 @@ def list_to_csv(data):
 def csv_to_list(csv:str, casting=float):
     if csv == '':
         return  []
-    res = csv.split(',')
-    return list(map(casting, res))
+    splited = csv.split(',')
+    res = []
+    for x in splited:
+        try:
+            x  = casting(x)
+            res.append(x)
+        except:
+            continue
+    #res = list(map(casting, res))
+    #res = list()
+    return res
